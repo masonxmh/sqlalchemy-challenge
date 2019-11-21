137489ee16d97b3793a70f99a8eac1fe6cf499a4
masonxmh <55073376+masonxmh@users.noreply.github.com>
2019-11-20 22:10:23 -0800

    update

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<'start'><br/>"
-        f"/api/v1.0/<'start'>/<'end'><br/>"
+        f"/api/v1.0/&ltstart&gt<br/>"
+        f"/api/v1.0/&ltstart&gt/&ltend&gt<br/>"
         
     )
 
